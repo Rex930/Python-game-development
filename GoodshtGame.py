a0c316e3c1333b383bb8d4ed98806c347a6ac091
Rex930 <rexadejuyigbe@gmail.com>
2026-04-18 19:47:10 -0400

    Add files via upload

---
+++ GoodshtGame.py
@@ -0,0 +1,32 @@
+import pgzrun
+from random import randint
+
+TITLE = "Good Shot Game"
+
+WIDTH = 500
+HEIGHT = 400
+
+message =""
+alien = Actor("alien")
+
+def draw():
+    screen.clear()
+    screen.fill(color = (128,0,0))
+    alien.draw()
+    screen.draw.text(message, center = (400,10), fontsize = 30)
+
+def place_alien():
+        alien.x = randint(50, WIDTH-50)
+    
+        alien.y = randint(50, WIDTH-50)
+def on_mouse_down(pos):
+        global message
+        if alien.collidepoint(pos):
+          message = "Good Shot!"
+          place_alien()
+        
+        else:
+            message = "You Missed!"
+        
+place_alien()
+pgzrun.go()
