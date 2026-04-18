7f1cab497713816828c4d6ce9ec59d84d5b7614e
Rex930 <rexadejuyigbe@gmail.com>
2026-04-18 19:04:10 -0400

    Add files via upload

---
+++ Favfruits.py
@@ -0,0 +1,26 @@
+person1 = {}
+person1['name'] = input("What is your name? ")
+person1['favorite_fruit'] = input("What is your favorite fruit? ")
+print("I like " +person1['favorite_fruit'] + " too!")
+print(person1)
+person2 = {}
+person2['name'] = input("What is your name? ")
+person2['favorite_fruit'] = input("What is your favorite fruit? ")
+print("I like " + person2['favorite_fruit'] + " too!")
+print(person2)
+person3 = {}
+person3['name'] = input("What is your name? ")
+person3['favorite_fruit'] = input("What is your favorite fruit? ")
+print("I like " + 3['favorite_fruit'] + " too!")
+print(person3)
+person4= {}
+person4['name'] = input("What is your name? ")
+person4['favorite_fruit'] = input("What is your favorite fruit? ")
+print("I like " + person44['favorite_fruit'] + " too!")
+print(person4)
+person5 = {}
+person5['name'] = input("What is your name? ")
+person5['favorite_fruit'] = input("What is your favorite fruit? ")
+print("I like " + person55['favorite_fruit'] + " too!")
+print(person5)
+print(Person1['name'], "likes", Person1['favorite_fruit'], "\n", Person2['name'], "likes", Person2['favorite_fruit'], "\n", Person3['name'], "likes", Person3['favorite_fruit'], "\n", Person4['name'], "likes", Person4['favorite_fruit'], "\n", Person5['name'], "likes", Person5['favorite_fruit']) 
