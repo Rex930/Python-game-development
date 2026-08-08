d4766b563b8f58751e26cb39a69c22666466a231
Rex930 <rexadejuyigbe@gmail.com>
2026-08-08 19:59:10 -0400

    Add files via upload

--- Tuple_3.py
+++ Tuple_3.py
@@ -33,4 +33,68 @@ quiz = (
      "C. /",
      "D. -",
      "B"),
+
+     ("6. Which keyword is used to create a function?",
+      "A. function",
+      "B. define",
+      "C. fun",
+      "D. def",
+      "D"),
+
+    ("7. Which loop is used to repeat a block of code a fixed number of times?",
+     "A. if",
+     "B. while",
+     "C. for",
+     "D. else",
+     "C"),
+
+     ("8. Which braclets are used to create a tuple?",
+      "A. []",
+      "B. {}",
+      "C. ()",
+      "D. <>",
+      "C"),
+
+      ("9. Which of the following is a Boolean value?",
+       "A. Hello",
+       "B. 100",
+       "C. True",
+       "D. 3.14",
+       "C"),
+
+       ("10. Which keyword is used to make decisions in python?",
+        "A. if",
+        "B. repeat",
+        "C. loop",
+        "D. switch",
+        "A")
 )
+
+score = 0
+print ("===== PYTHONBASICS QUIZ=====")
+
+for question in quiz:
+    print("\n" + question[0])
+    print(question[1])
+    print(question[2])
+    print(question[3])
+    print(question[4])
+    answer = input("Your answer (A/B/C/D): ").upper()
+    if answer == question[5]:
+        print("Correct!")
+        score += 1
+    else:
+        print("Wrong!")
+        print("Correct Answer:", question[5])
+
+print("\n========= QUIZ OVER =========")
+print("Your Score:", score, "/" , len(quiz))
+
+percentage = (score / len(quiz)) * 100
+print("Percentage:", percentage, "%")
+
+if percentage == 100:
+    print("Excellent! You got a perfect score!")
+    elif
+
+print(f"\nYour final score is: {score}/{len(quiz)}")
